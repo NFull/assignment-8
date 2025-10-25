e1198468ffeded85e4807526822107d82ca25aad
NFull <natalie.fullerton530@gmail.com>
2025-10-25 13:56:12 -0400

    storing each answer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 console.log("script.js connected!");
+
+
+let buttons = document.querySelectorAll("#questions button");
+
+let userAnswers = {}
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    	
+    let buttonID = button.dataset.buttonid
+    let response = button.dataset.answer;
+    userAnswers[buttonID] = response;
+    console.log(userAnswers);
+    
+  });
+});
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    buttons.forEach(function(btn) {
+      btn.classList.remove("selected");
+    });
+    button.classList.add("selected");
+    
+    
+    let answer = button.getAttribute("data-answer");
+    
+  });
+});
+
